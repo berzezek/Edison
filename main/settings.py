ad0df26b5514df365c6b423d4943061e862b76e1
Berzezek <wknduz@gmail.com>
2022-01-07 22:04:42 +0500

    heroku deploy

--- main/settings.py
+++ main/settings.py
@@ -121,7 +121,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # STATICFILES_DIRS = ((BASE_DIR / 'react-psychic/build/static'), )
-STATICFILES_DIRS = ((BASE_DIR / 'staticfiles'), )
+# STATICFILES_DIRS = ((BASE_DIR / 'staticfiles'), )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
